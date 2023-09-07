9f135f1519dfbad930812502e0a9db2e7df10493
Zykov Vladimir <flesch.vz@mail.ru>
2023-09-07 11:49:38 +0700

    Реализованы конструкторы и деструктор

--- BoolVector/BoolVector.cpp
+++ BoolVector/BoolVector.cpp
@@ -1,4 +1,37 @@
 ﻿#include "BoolVector.h"
 
 
+BoolVector::BoolVector()
+{
+	m_additionInLength = sizeof(char);
+	m_realLen = 0;
+	m_maxLen = m_realLen + m_additionInLength;
 
+	m_bits(nullptr, 0, 1, 1);
+}
+
+BoolVector::BoolVector(const char* mas, const int len)
+{
+	m_additionInLength = sizeof(char);
+	m_realLen = 0;
+	m_maxLen = m_realLen + m_additionInLength;
+
+	m_bits(mas, len, 1, 1);
+}
+
+BoolVector::BoolVector(const BoolVector otherVector)
+{
+	m_additionInLength = sizeof(char);
+	m_realLen = otherVector.m_realLen;
+	m_maxLen = otherVector.m_maxLen;
+
+	m_bits(otherVector.m_bits, m_realLen, 1, 1);
+}
+
+BoolVector::BoolVector(const int len)
+{
+	m_realLen = len;
+	m_maxLen = m_realLen + m_additionInLength;
+	
+	m_bits(nullptr, m_realLen, 1, 1);
+}
--- BoolVector/BoolVector.h
+++ BoolVector/BoolVector.h
@@ -1,19 +1,49 @@
 #include "..\Array\TemplateArray.h"
+#include <iostream>
+
 
 class BoolVector
 {
 public:
 	BoolVector();
-	BoolVector(const char* mas);
+	BoolVector(const char* mas, const int len);
 	BoolVector(const BoolVector otherBoolVector);
 	BoolVector(const int len);
-	
+	~BoolVector() = default;
+
+	friend std::ostream operator << (std::ostream vectorOut, BoolVector vector);
+	friend std::istream operator >> (std::istream vectorIn, BoolVector vector);
+
+	void inversion();
+	void bitInvertion(int index);
+
+	void bitSet(int index, int value);
+	void bitReplacment(int index, int value);
+	void vectorReplacement(int value);
+
+	int vectorWeight() const;
+
+	int operator [](int index);
+	BoolVector operator & (const BoolVector otherVector) const;
+	BoolVector& operator &= (const BoolVector otherVector);
+	BoolVector operator | (const BoolVector otherVector) const;
+	BoolVector& operator &= (const BoolVector otherVector);
+	BoolVector operator ^ (const BoolVector otherVector) const;
+	BoolVector& operator ^= (const BoolVector otherVector);
+	BoolVector operator << (const int step) const;
+	BoolVector& operator  <<= (const int step);
+	BoolVector operator >> (const int step) const;
+	BoolVector& operator >>= (const int step);
+	BoolVector& operator ~ ();
+	BoolVector& operator = (const BoolVector otherVector);
 
 private:
-	cosnt int m_additionInLength = sizeof(char);
+	const int m_additionInLength = sizeof(char);
 
 	TemplateArray<char> m_bits;
 	int m_realLen = 0;
-	int m_maxLen = m_realLen + m_additionInLength
+	int m_maxLen = m_realLen + m_additionInLength;
+
+	bool indexAdmissable(int index);
 	
 };
